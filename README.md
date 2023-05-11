77035d8129248f385a0beb5b318d62f20a17c7ed
tmax818 <tylermaxwell661@gmail.com>
2023-05-11 16:23:06 -0600

    it works, I think...

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # mern_express_mongoose_reading
 
 - made with nodejs in webstorm
+
+```bash
+npm init -y
+npm install mongoose express dotenv nodemon
+```
--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log(this)
+const fs = require('fs')
+
+const files = ['config', 'controllers', 'models', 'routes']
+
+files.forEach(file => {
+fs.mkdirSync(`${__dirname}/server/${file}/`, {recursive: true})
+})
+
